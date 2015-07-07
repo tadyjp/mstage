5b401af11e9b48dfb54b3b4bd0d156e409e4b55f
tady <a.dat.jp@gmail.com>
2015-07-07 01:00:28 +0000

    WIP

--- bin/run_router.sh
+++ bin/run_router.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-docker rm -f router-redis; \
-  docker run --name router-redis -p 6379:6379 -d redis:latest
+docker rm -f router-redis
+docker run --name router-redis -p 6379:6379 -d redis:latest
 
-docker rm -f router-web; \
-  docker run --name router-web -p 80:80 --link router-redis:redis -d tadyjp/router:1
+docker rm -f router-web
+docker run --name router-web -p 80:80 --link router-redis:redis -d tadyjp/router:1
 
